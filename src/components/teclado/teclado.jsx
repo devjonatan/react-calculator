fdb7f41a634441073bfd5734506eccc2ad57d812
Jonatan Waldow <jonwldw@gmail.com>
2021-03-27 15:11:49 -0400

    Importação de estilo de botão para o teclado

--- src/components/teclado/teclado.jsx
+++ src/components/teclado/teclado.jsx
@@ -2,9 +2,32 @@ import { Component } from "react";
 import "./style.css";
 
 class Teclado extends Component {
+
+	constructor() {
+		super();
+
+		this.teclas = [];
+
+		const teclas = "1234567890.,+-/*=";
+		for (let indice = 0; indice < teclas.length; indice++) {
+			const tecla = teclas[indice];
+			this.teclas.push(tecla);
+		}
+	}
+
 	render() {
 		return (
-			<div>Teclado aqui!</div>
+			<div className="teclado">
+				{this.teclas.map((tecla, index) => {
+					return (
+						<button className="button" key={index}>
+							<div className="button__content">
+								<p className="button__text">{tecla}</p>
+							</div>
+						</button>
+					)
+				})}
+			</div>
 		)
 	}
 }
